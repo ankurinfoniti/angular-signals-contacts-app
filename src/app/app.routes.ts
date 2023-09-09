2060c8557f6ffb62873b5a9e706d9d4e86b2d654
ankurinfoniti <ankur.infoniti@gmail.com>
2023-09-09 11:51:42 +0530

    all component, service, model and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: ContactsListComponent,
+  },
+  {
+    path: 'add',
+    component: AddContactComponent,
+  },
+];
